31d4d5d91248353051d5a339899424391043b228
Ted Li <tl47704n@pace.edu>
2017-04-05 16:29:01 -0400

    fixed upload&search

---
+++ app/src/main/java/com/example/pc/ChineseChow/Search.java
@@ -0,0 +1,101 @@
+package com.example.pc.ChineseChow;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class Search extends AppCompatActivity {
+
+
+    private DatabaseReference databaseReference;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> recipeNames = new ArrayList<String>();
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+         ListView lv = (ListView) findViewById(R.id.ListView);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, recipeNames);
+
+
+        DatabaseReference databaseReference =  FirebaseDatabase.getInstance().getReferenceFromUrl("https://homechefparty-e0f77.firebaseio.com/Recipes");
+
+
+        databaseReference.orderByChild("recipeName").addChildEventListener(new ChildEventListener() {
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Recipe value = dataSnapshot.getValue(Recipe.class);
+
+                recipeNames.add(value.getRecipeName());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        lv.setAdapter(adapter);
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.menuSearch));
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
---
+++ app/src/main/java/com/example/pc/ChineseChow/Upload.java
@@ -0,0 +1,141 @@
+package com.example.pc.ChineseChow;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.sql.Array;
+import java.util.List;
+
+/**
+ * Created by pc on 2017/3/19.
+ */
+
+public class Upload extends Activity {
+    Button recipe_upload;
+    EditText ingredients;
+    EditText recipeSteps;
+    EditText recipe_name;
+    EditText prepTime;
+    EditText cookTime;
+    Firebase main_ref;
+    ImageButton selectimage;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReference();
+
+    private static final int imagerequest = 1;
+    private Uri mimageUri=null;
+    private StorageReference mStorage;
+    private ProgressDialog mProgress;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        selectimage = (ImageButton)findViewById(R.id.bt_addimage);
+
+        main_ref = new Firebase("https://homechefparty-e0f77.firebaseio.com/");
+        recipe_upload = (Button)findViewById(R.id.bt_upload);
+        recipe_name = (EditText)findViewById(R.id.et_recipe);
+        cookTime = (EditText) findViewById(R.id.cookTime);
+        prepTime = (EditText) findViewById(R.id.prepTime);
+        recipeSteps = (EditText) findViewById(R.id.recipeSteps);
+        ingredients = (EditText) findViewById(R.id.ingredients);
+        selectimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent gallaryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                gallaryIntent.setType("image/*");
+                startActivityForResult(gallaryIntent, imagerequest);
+            }
+        });
+
+
+        recipe_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mimageUri!=null){
+                    StorageReference filepath =  mStorage.child("Recipe_Images").child(mimageUri.getLastPathSegment());
+                    filepath.putFile(mimageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            @SuppressWarnings("VisibleForTests")Uri downloadUri = taskSnapshot.getDownloadUrl();
+                            databaseReference.child("Recipes").push().setValue(downloadUri.toString());
+                        }
+                    });
+                }
+
+                String getrecipeName = recipe_name.getText().toString();
+                String getcookTime = cookTime.getText().toString();
+                String getprepTime = prepTime.getText().toString();
+                String getRecipeSteps = recipeSteps.getText().toString();
+                String getIngredient = ingredients.getText().toString();
+
+                Recipe r = new Recipe();
+
+                r.setIngredients(getIngredient);
+                r.setRecipeName(getrecipeName);
+                r.setCookTime(getcookTime);
+                r.setPrepTime(getprepTime);
+                r.setRecipeSteps(getRecipeSteps);
+                databaseReference.child("Recipes").push().setValue(r);
+
+
+
+                ;
+            }
+        });
+
+
+
+
+    }
+    private void startposting() {
+
+        mProgress.setMessage("Now Posting....");
+        mProgress.show();
+
+        String getrecipeNameforPicture = recipe_name.getText().toString();
+        if(!TextUtils.isEmpty(getrecipeNameforPicture)&& mimageUri!=null){
+            StorageReference filepath =  mStorage.child("Recipe_Images").child(mimageUri.getLastPathSegment());
+            filepath.putFile(mimageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    @SuppressWarnings("VisibleForTests")Uri downloadUri = taskSnapshot.getDownloadUrl();
+                    mProgress.dismiss();
+                }
+            });
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == imagerequest && resultCode==RESULT_OK){
+            mimageUri = data.getData();
+            selectimage.setImageURI(mimageUri );
+        }
+    }
+
+}
+
